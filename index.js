e388ba313cd716e17b1061f61d6f0106391bc399
BincomAD <100017314+BincomAD@users.noreply.github.com>
2023-09-16 21:49:03 +0300

    first version

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-const root = document.getElementById("root")
+const root = document.getElementById("root");
+const body = document.body
 
-const menu = document.createElement("a")
-menu.textContent = "Клик"
-
-root.appendChild(menu)
-
-menu.onclick = function() {
-    renderModuleMenu()
+const state = {
+    active: false,
 }
 
+const menu = document.createElement("a");
+menu.textContent = "Клик";
+menu.classList.add("Click");
+
+root.appendChild(menu);
+
+menu.addEventListener('click', () => {
+    renderModuleMenu();
+    state.active = !state.active;
+})
+
+document.addEventListener('click', (e) => {
+    const click = document.querySelector('.Click')
+
+    if (state.active === true) {
+        const moduleMenu = document.querySelector('.ModuleMenu');
+        const array = Array.from(moduleMenu.childNodes)
+        if (e.target !== click &&  e.target !== moduleMenu) {
+            moduleMenu.remove()
+            state.active = !state.active;
+        }
+    }
+})
+
 function renderModuleMenu() {
-    const moduleMenu = document.CreateElement("div")
-    
+    if (state.active === false) {
+        const moduleMenu = document.createElement("div");
+        moduleMenu.classList.add("ModuleMenu");
+        root.appendChild(moduleMenu);
+
+        const text = document.createElement("span")
+        text.textContent = "Какой-то текст в модальном окне"
+        moduleMenu.appendChild(text)
+
+    } else {
+        const moduleMenu = document.querySelector('.ModuleMenu');
+        moduleMenu.remove();
+    }
 }
